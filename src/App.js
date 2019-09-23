f1f12515584e98adebe88a45ea3b7dd1f5150776
Gustavo <gustavo.gigante.s@gmail.com>
2019-09-23 23:02:40 +0100

    Slight code refactor

--- src/App.js
+++ src/App.js
@@ -10,170 +10,129 @@ import Dashboard from './Dashboard/Dashboard.js';
 import Signup from './Signup/Signup.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      showLogin: true,
-      showScreen1: false,
-      showRegister: false,
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			showLogin: true,
+			showScreen1: false,
+			showRegister: false,
+		}
+	}
 
-  handleChangeEmail = (evt) => {
-    this.setState({
-      email: evt.target.value
-    })
-  }
+	handleChangeEmail = (evt) => {
+		this.setState({
+			email: evt.target.value
+		})
+	}
 
-  handleChangePassword = (evt) => {
-    this.setState({
-      password: evt.target.value
-    })
-  }
+	handleChangePassword = (evt) => {
+		this.setState({
+			password: evt.target.value
+		})
+	}
 
-  showRegister = () => {
-    let prevState = this.state;
-    prevState.showRegister = true;
-    prevState.showLogin = false;
-    this.setState({
-      prevState,
-    })
-  }
+	showRegister = () => {
+		let prevState = this.state;
+		prevState.showRegister = true;
+		prevState.showLogin = false;
+		this.setState({
+			prevState,
+		})
+	}
 
-  closeSignupScreen = () => {
-    let prevState = this.state;
-    prevState.showRegister = false;
-    prevState.showLogin = true;
-    this.setState({
-      prevState,
-    })
-  }
+	closeSignupScreen = () => {
+		let prevState = this.state;
+		prevState.showRegister = false;
+		prevState.showLogin = true;
+		this.setState({
+			prevState,
+		})
+	}
 
-  submitState = (e) => {
-    e.preventDefault();
-    let prevState = this.state;
-    if(prevState.email !== '') {
-      Firestore.auth().signInWithEmailAndPassword(prevState.email, prevState.password)
-      .then(function() {
-        prevState.showLogin = false;
-        prevState.showScreen1 = true;
-        updateScreenState();
-      })
-      .catch(function() {
-        console.log("User Doesnt' Exists")
-        //document.getElementById("loginWarning").style.display = "block";
-      })
+	submitState = (e) => {
+		e.preventDefault();
+		let prevState = this.state;
+		if(prevState.email !== '') {
+			Firestore.auth().signInWithEmailAndPassword(prevState.email, prevState.password)
+			.then(function() {
+				prevState.showLogin = false;
+				prevState.showScreen1 = true;
+				updateScreenState();
+			})
+			.catch(function() {
+				console.log("User Doesnt' Exists");
+				//document.getElementById("loginWarning").style.display = "block";
+			})
 
-      let updateScreenState = () => {
-        this.setState({
-          prevState,
-        })
-      }
-    }
-    
-  }
+			let updateScreenState = () => {
+				delete prevState.password;
+				this.setState({
+					prevState,
+				});
+				console.log(this.state);
+			}
+		}
+	}
 
-  registerNewUser = (e) => {
-    e.preventDefault();
-    let prevState = this.state;
-    const db = Firestore.firestore();
-    Firestore.auth().signInWithEmailAndPassword(prevState.email, prevState.password) //Check if use exists
-    .then(function(firebaseUser) {
-        console.log("User already exists"); 
-    })
-    .catch(function(error) {
-      Firestore.auth().createUserWithEmailAndPassword(prevState.email, prevState.password).catch(function(error) { //Create new user
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
-    });
+	registerNewUser = (e) => {
+		e.preventDefault();
+		let prevState = this.state;
+		const db = Firestore.firestore();
+		Firestore.auth().signInWithEmailAndPassword(prevState.email, prevState.password) //Check if user exists
+		.then(function(firebaseUser) {
+			console.log("User already exists"); 
+		})
+		.catch(function(error) {
+			Firestore.auth().createUserWithEmailAndPassword(prevState.email, prevState.password);
+			let data = {
+				'email' : prevState.email
+			}
+			db.collection('users').doc(prevState.email).set(data);
+		});
 
-    let data = {
-      'email' : prevState.email
-    }
+		this.setState({
+			email: '',
+			showLogin: true,
+			showScreen1: false,
+			showRegister: false,
+		});
+	}
 
-    db.collection('users').doc(prevState.email).set(data);
-    db.collection('users').doc(prevState.email).collection('spents').doc('car').set('carro');
+	render() {
+		return (
+			<div className="App">
+				{/* #Register Screen */
+					this.state.showRegister ? [
+						<Signup
+							email={this.handleChangeEmail}
+							passwordChange={this.handleChangePassword}
+							closeSignupScreen = {this.closeSignupScreen}
+							closePopup = {this.closeSignupScreen}
+							registerUser = {this.registerNewUser}
+						/>
+					] : null
+				}
 
+				{/* #Login Screen */
+					this.state.showLogin ?  [
+					<Login
+						email={this.handleChangeEmail}
+						passwordChange={this.handleChangePassword}
+						submitState={this.submitState}
+						signUpClick={this.showRegister}
+						ref={this.warning}/>
+					]: null
+				}
 
-    this.setState({
-      email: '',
-      showLogin: true,
-      showScreen1: false,
-      showRegister: false,
-    })
-
-  }
-
-  teste = () => {
-    const db = Firestore.firestore();
-    db.collection("users").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-         // console.log(`${doc.id} => ${doc.data().email}`);
-      });
-    });
-
-    let a = db.collection('users').doc('gustavo.gigante.@gmail.com').collection('spents').doc('teste');
-    console.log(a);
-
-    var citiesRef = db.collection('users').doc('gustavo.gigante.@gmail.com').collection('spents');
-    var allCities = citiesRef.get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          console.log(doc.id, '=>', doc.data());
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
-      });
-
-
-    //TODO: Restructure data structure...
-  }
-
-  render() {
-    return (
-      <div className="App">
-        {/* #Register Screen */}
-        {
-          this.state.showRegister ? [
-            <Signup
-              email={this.handleChangeEmail}
-              passwordChange={this.handleChangePassword}
-              closeSignupScreen = {this.closeSignupScreen}
-              closePopup = {this.closeSignupScreen}
-              registerUser = {this.registerNewUser}
-            />
-          ] : null
-        }
-
-        {/* #Login Screen */}
-        {
-          this.state.showLogin ?  [
-            this.teste(),
-            <Login
-              email={this.handleChangeEmail}
-              passwordChange={this.handleChangePassword}
-              submitState={this.submitState}
-              signUpClick={this.showRegister}
-              ref={this.warning}/>
-          ]: null
-        }
-
-        {/* #Dashboard Screen */}
-        {
-          this.state.showScreen1 ? [
-
-            <Dashboard />,
-            this.state.email,
-          ] : null
-        }
-      </div>
-    );
-  }
+				{/* #Dashboard Screen */
+					this.state.showScreen1 ? [
+					<Dashboard />,
+						this.state.email,
+					] : null
+				}
+			</div>
+		);
+	}
 }
 
 export default App;
